ef5f658dd6061e4a2c27e1b2ba8af0acc464ed5f
Tom Chen <tomchen.org@gmail.com>
2021-01-24 13:29:28 +0100

    init

---
+++ README.md
@@ -0,0 +1,66 @@
+# typedoc-plugin-not-exported
+
+[![npm package](https://img.shields.io/badge/npm%20i-example--typescript--package-brightgreen)](https://www.npmjs.com/package/typedoc-plugin-not-exported) [![version number](https://img.shields.io/npm/v/typedoc-plugin-not-exported?color=green&label=version)](https://github.com/tomchen/typedoc-plugin-not-exported/releases) [![License](https://img.shields.io/github/license/tomchen/typedoc-plugin-not-exported)](https://github.com/tomchen/typedoc-plugin-not-exported/blob/main/LICENSE)
+
+This typedoc plugin can force inclusion of specific symbols (variables) that are not exported, by making them fake exports.
+
+Install the plugin:
+
+```bash
+npm i typedoc-plugin-not-exported
+```
+
+In your code, tag the symbols (variables / types / interfaces / classes etc.) that are not exported but you still want to include in the generated documentation.
+
+The default tag is `@notExported`.
+
+Example 1:
+
+```ts
+/**
+ * My class
+ * @notExported
+ */
+class Cls {
+  convert(str: string): string {
+    return str
+  }
+}
+export const me = new Cls()
+```
+
+Example 2:
+
+```ts
+/**
+ * @notExported
+ */
+type twoNumbers = [number, number]
+/**
+ * @notExported
+ */
+type threeNumbers = [number, number, number]
+
+export type twoOrThreeNumbers = twoNumbers | threeNumbers
+export function sum2(ns: twoOrThreeNumbers): number {
+  return ns.reduce((a, b) => a + b)
+}
+```
+
+Then use the command as usual:
+
+```bash
+typedoc src/index.ts
+```
+
+Or, if you are using `@internalDoNotUse` tag instead of `@notExported`, run:
+
+```bash
+typedoc --includeTag internalDoNotUse src/index.ts
+```
+
+That's it.
+
+Originally from [here](https://github.com/TypeStrong/typedoc/issues/1474#issuecomment-766178261). [CC0](LICENSE).
+
+**Keywords: typedoc plugin force include non exported unexported variable symbol fake export option flag tag mode file exclude inclusion internal external**
---
+++ src/main.js
@@ -0,0 +1,80 @@
+/**
+ * typedoc-plugin-not-exported
+ * TypeDoc plugin that forces inclusion of non-exported symbols (variables)
+ * Originally from https://github.com/TypeStrong/typedoc/issues/1474#issuecomment-766178261
+ * https://github.com/tomchen/typedoc-plugin-not-exported
+ * CC0
+ */
+
+//@ts-check
+
+const { Converter, TypeScript } = require('typedoc') // version 0.20.x
+
+const ModuleFlags =
+  TypeScript.SymbolFlags.ValueModule | TypeScript.SymbolFlags.NamespaceModule
+
+/** @param {{ application: import("typedoc").Application }} param0 */
+exports.load = function ({ application }) {
+  let includeTag = 'notExported'
+
+  application.options.addDeclaration({
+    name: 'includeTag',
+    help:
+      '[typedoc-plugin-not-exported] Specify the tag name for non-exported member to be imported under',
+    defaultValue: includeTag,
+  })
+
+  application.converter.on(Converter.EVENT_BEGIN, () => {
+    includeTag = application.options.getValue('includeTag').toLocaleLowerCase()
+  })
+
+  application.converter.on(
+    Converter.EVENT_CREATE_DECLARATION,
+    lookForFakeExports
+  )
+
+  /**
+   * @param {import("typedoc/dist/lib/converter/context").Context} context
+   * @param {import("typedoc").DeclarationReflection} _reflection
+   * @param {TypeScript.Node | undefined} node
+   */
+  function lookForFakeExports(context, _reflection, node) {
+    if (!node) {
+      return
+    }
+
+    /** @type {TypeScript.Symbol | undefined} */
+    const moduleSymbolTemp = context.checker.getSymbolAtLocation(node)
+    const moduleSymbol =
+      moduleSymbolTemp !== null && moduleSymbolTemp !== undefined
+        ? moduleSymbolTemp
+        : /** @type {any} */ node.symbol
+
+    if (!moduleSymbol) {
+      // Global file, no point in doing anything here. TypeDoc will already
+      // include everything declared in this file.
+      return
+    }
+
+    // Make sure we are allowed to call getExportsOfModule
+    if ((moduleSymbol.flags & ModuleFlags) === 0) {
+      return
+    }
+
+    const exportedSymbols = context.checker.getExportsOfModule(moduleSymbol)
+
+    const symbols = context.checker
+      .getSymbolsInScope(node, TypeScript.SymbolFlags.ModuleMember)
+      .filter((symbol) => !exportedSymbols.includes(symbol))
+
+    for (const symbol of symbols) {
+      if (
+        symbol
+          .getJsDocTags()
+          .some((tag) => tag.name.toLocaleLowerCase() === includeTag)
+      ) {
+        context.converter.convertSymbol(context, symbol)
+      }
+    }
+  }
+}
